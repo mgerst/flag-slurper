7ef9911890bb2e4fb342a1d4e01b4a61b2b8cc15
Matt Gerst <mattgerst@gmail.com>
2018-02-01 17:29:10 -0600

    Add some tests for plant cli command.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,6 @@
+from unittest import mock
+
+import pytest
 from click.testing import CliRunner
 
 from flag_slurper.cli import cli, pass_conf
@@ -27,3 +30,22 @@ def test_cli_pass_config():
     @pass_conf
     def cmd(conf):
         assert conf == Config.get_instance()
+
+    runner = CliRunner()
+    runner.invoke(cli, ['cmd'])
+
+
+def test_plant_invalid_user():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['plant'])
+    assert result.exit_code == 1
+
+
+@pytest.mark.skip('Mocks not working')
+@mock.patch('flag_slurper.utils.check_user')
+def test_plant(check_user):
+    check_user.return_value = None
+    runner = CliRunner()
+    result = runner.invoke(cli, ['plant'])
+    assert check_user.called
+    assert result.exit_code == 0
