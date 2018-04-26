c3f221888095270729a91078a853254c427efbb1
Matt Gerst <mattgerst@gmail.com>
2018-04-26 18:25:37 -0500

    Add tests for login command.

--- flag_slurper/config.py
+++ flag_slurper/config.py
@@ -101,13 +101,18 @@ def config():
 
 @config.command()
 @click.option('-t', '--token', prompt=True, hide_input=True, help='An explicit api token')
-def login(token):
-    flagrc = Path('~/.flagrc')
+@click.option('-c', '--config', default='~/.flagrc', help='flagrc to save token to')
+def login(token, config):
+    flagrc = Path(config)
     click.echo("Writing token to {}".format(flagrc))
     flagrc = flagrc.expanduser()
 
     c = ConfigParser()
     c.read(str(flagrc))
+
+    if not c.has_section('iscore'):
+        c.add_section('iscore')
+
     c['iscore']['api_token'] = token
     with flagrc.open('w') as fp:
         c.write(fp)
