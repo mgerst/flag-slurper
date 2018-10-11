56d98cb691fa877cfd44b67dc9014e140e419659
Matt Gerst <mattgerst@gmail.com>
2018-10-10 23:22:13 -0500

    Add tests for post configuration in project

--- tests/autolib/test_post.py
+++ tests/autolib/test_post.py
@@ -6,24 +6,6 @@ from schema import SchemaError, Optional
 from flag_slurper.autolib import post
 
 
-# def test_pos_ssh(mock, sudocred):
-#     """
-#     TODO This clearly needs to be a lot better.
-#     """
-#     ssh = mock.MagicMock()
-#     mock.patch('flag_slurper.autolib.post.get_directory')
-#
-#     run_sudo = mock.patch('flag_slurper.autolib.post.run_sudo')
-#     run_sudo.return_value = [StringIO(), BytesIO(b""), BytesIO()]
-#
-#     ssh.exec_command.return_value = [StringIO(), BytesIO(b""), BytesIO()]
-#
-#     post.post_ssh(ssh, sudocred)
-#
-#     assert run_sudo.called
-#     assert ssh.exec_command.called
-
-
 class TestPlugin(post.PostPlugin):
     name = 'test'
     schema = {
