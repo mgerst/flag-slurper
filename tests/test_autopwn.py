c4ec0a8cafab2577b8288e5124c3274a3f2f78d0
Matt Gerst <mattgerst@gmail.com>
2023-09-30 00:36:52 -0500

    Add tests for autopwn generate

--- tests/test_autopwn.py
+++ tests/test_autopwn.py
@@ -1,7 +1,10 @@
 import pytest
+import vcr
 from click.testing import CliRunner
 
+from flag_slurper.autolib.models import Team
 from flag_slurper.cli import cli
+from flag_slurper.conf import Config
 from flag_slurper.conf.project import Project
 
 
@@ -15,6 +18,14 @@ def pwn_project(create_project):
     p = Project.get_instance()
     p.load(str(tmpdir.join('project.yml')))
     return str(tmpdir.join('project.yml'))
+
+
+@pytest.fixture
+def custom_config():
+    old_config = Config.get_instance()
+    Config.instance = None
+    yield Config.get_instance()
+    Config.instance = old_config
 
 
 def test_autopwn_no_project():
@@ -50,3 +61,31 @@ def test_autopwn_pwn_random(pwn_project, mocker):
     result = runner.invoke(cli, ['autopwn', 'pwn', '-r'])
     assert result.exit_code == 0
     random.assert_called()
+
+
+@vcr.use_cassette('fixtures/autopwn_generate.yaml')
+def test_autopwn_generate(pwn_project):
+    _clear_teams()
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ['autopwn', 'generate'])
+
+    assert result.exit_code == 0
+    assert Team.select().count() == 18
+
+
+@vcr.use_cassette('fixtures/autopwn_generate.yaml')
+def test_autopwn_generate_ignore_guest(pwn_project, custom_config):
+    custom_config['iscore']['ignore_guest_division'] = 'true'
+
+    _clear_teams()
+    runner = CliRunner()
+    result = runner.invoke(cli, ['autopwn', 'generate'])
+
+    assert result.exit_code == 0
+    assert Team.select().count() == 18
+
+
+def _clear_teams():
+    for team in Team.select():
+        team.delete_instance()
